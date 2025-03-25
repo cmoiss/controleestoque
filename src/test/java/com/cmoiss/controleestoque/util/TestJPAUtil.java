af79c627dc544b69928abc980001b1153f073c49
Caio Souza <ocaiomoises@gmail.com>
2025-03-25 13:50:27 -0300

    Create TestJPAUtil.java

---
+++ src/test/java/com/cmoiss/controleestoque/util/TestJPAUtil.java
@@ -0,0 +1,31 @@
+package com.cmoiss.controleestoque.util;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestJPAUtil {
+
+    private static final String PERSISTENCE_UNIT_NAME = "h2PU";
+    private static EntityManagerFactory emf;
+
+    public static EntityManager createTestEntityManager() {
+        if (emf == null) {
+            Map<String, String> properties = new HashMap<>();
+            properties.put("javax.persistence.jdbc.url", "jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1");
+            properties.put("javax.persistence.jdbc.user", "sa");
+            properties.put("javax.persistence.jdbc.password", "");
+            properties.put("hibernate.hbm2ddl.auto", "create-drop"); // Limpa o banco após cada teste
+            emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME, properties);
+        }
+        return emf.createEntityManager();
+    }
+
+    public static void closeEntityManagerFactory() {
+        if (emf != null && emf.isOpen()) {
+            emf.close();
+        }
+    }
+}
